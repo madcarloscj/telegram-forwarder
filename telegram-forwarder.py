05ec304ec2ef093e7c872d5bab7b60cdc5d506e9
madcarloscj <82256087+madcarloscj@users.noreply.github.com>
2024-03-27 22:42:53 +0100

    Added offset id
    Added the offset id in a file so that this can be triggered automatically and it uses the last offset available

--- telegram-forwarder.py
+++ telegram-forwarder.py
@@ -33,8 +33,8 @@ GROUP_DESTINATION = {NAME_OF_YOUR_DESTINATION_GROUP}
 # Define an async function to get the forward the messages from the source group to the destination group
 async def copy_group_to_group ():
   
-  # Connect the client to the Telegram server
-  await client.connect ()
+	# Connect the client to the Telegram server
+	await client.connect ()
   
   # Check if the client is connected
   if not client.is_connected ():
@@ -68,58 +68,81 @@ async def copy_group_to_group ():
     
     # If not, raise an exception
     raise ValueError ('Cannot find any entity corresponding to ' + GROUP_2)
-    
 
-  # In which message you want to start
-  offset_id = 1 # Initial offset value
-  limit = 10 # Number of messages to fetch per batch
+	# Open the file in read mode
+	# This file stores the last offset so that next time that you execute the code
+	# the application continues from that offset id
+	try:
+		with open(GROUP_DESTINATION+"_offset.log", 'r') as file:
+			# Read the contents of the file
+			offset_str = file.read()
+	except (FileNotFoundError, ValueError):
+  	with open(GROUP_DESTINATION+"_offset.log", 'w') as file:
+    	file.write('1')
+    	offset_str = "1"
+
+  # Convert the offset back to an integer
+  offset_id = int(offset_str)
+  limit = 100 # Number of messages to fetch per batch
   count = 0 # Count of messages copied
-  while True:
-    try:
-        # We get a batch with a {limit} number of messages starting from the offset where we are
-        print("Getting new batch with offset " + str(offset_id))
-        
-        # Get a batch of messages from the source group
-        # The reverse parameter is to get the messages from lower to higher id so that the groups are copied same as they are
-        batch = await client.get_messages(entity1, limit=limit, offset_id=offset_id, reverse=True)
-        
-        # If the batch is empty, break the loop print the total number of messages copied and leave
-        if not batch:
-          print("Total count: " + str(count))
-          break
 
-        # Go through all the messages in the batch
-        for message in batch:
-            count += 1
-            print("Sending message: " + str(message.id))
-
-            # Send the message to the destination group
-            # The message will be sent without sender, if you want to keep the sender you should use the forward_message option
-            await client.send_message(entity2, message)
-
-            # The offset should be updated with the latest message.id that was used
-            offset_id = message.id
-            logger.debug(str(message.id))
-        
-        # There is a limit on the Telegram API on the number of request and due to that a wait must be implemented
-        time.sleep(30)
+	try:
+      # Get an initial of messages from the source chat
+      batch = await client.get_messages(entity1, limit=limit, offset_id=offset_id, reverse=True)
     except errors.FloodWaitError as e:
-      # Catch the FloodWaitError
-      print(f'A wait of {e.seconds} seconds is required')
-      # Sleep for the required time
-      time.sleep(e.seconds)
+			# Catch the FloodWaitError
+			print(f'A wait of {e.seconds} seconds is required')
+			# Sleep for the required time
+			time.sleep(e.seconds)
+			batch = await client.get_messages(entity1, limit=limit, offset_id=offset_id, reverse=True)
 
-  print("Copy completed")
-  logger.debug("Copy completed")
+	# Keep getting batches until there are no more messages left in the source group
+	while batch:
+			for message in batch:
+					count += 1
+					print("Sending message: " + str(message.id))
+					try:
+							await client.send_message(entity2, message)
+							offset_id = message.id
+							logger.debug(str(message.id))
+					except errors.FloodWaitError as e:
+							# Catch the FloodWaitError
+							print(f'A wait of {e.seconds} seconds is required')
+							# Sleep for the required time
+							time.sleep(e.seconds)
+							print(f'The wait is now finished')
+			try:
+					# Get a new batch of messages from the source chat
+					batch = await client.get_messages(entity1, limit=limit, offset_id=offset_id, reverse=True)
+			except errors.FloodWaitError as e:
+					# Catch the FloodWaitError
+					print(f'A wait of {e.seconds} seconds is required')
+					# Sleep for the required time
+					time.sleep(e.seconds)
+					print(f'The wait is now finished so we can retry to get the message')
+					batch = await client.get_messages(entity1, limit=limit, offset_id=offset_id, reverse=True)
+			if not batch:
+					print("Total count: " + str(count))
+					print("Offset id: " + str(offset_id))
+					
+					# Open a file in write mode ('w') which will truncate the file
+					with open(GROUP_DESTINATION+"_offset.log", 'w') as file:
+							# Write the number to the file
+							file.write(str(offset_id))
+					sys.exit()
+			else:
+					print("Finished")
 
 # Run the async function using asyncio
 import asyncio
 try:
   # Try to run the function
   asyncio.run (copy_group_to_group ())
+  sys.exit()
 except (ConnectionError, ValueError) as e:
   # Handle the errors
   print (e)
+  sys.exit()
 
 
 
